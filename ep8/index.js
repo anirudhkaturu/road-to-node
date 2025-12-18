7ddd56e8831df335d1531c8368537621d2144a49
thesocalledhonouredone <katuruanirudh1@gmail.com>
2025-12-18 06:34:59 +0530

    handleing urls by parsing

--- ep8/index.js
+++ ep8/index.js
@@ -1,5 +1,21 @@
+// https://www.youtube.com/watch?v=Nt-AsZh5woE&list=PLinedj3B30sDby4Al-i13hQJGQoRQDfPo&index=8
+
+/*
+
+BASIC STRUCTURE OF A URL (Uniform Resource Locator)
+
+1. protocol: https
+2. domain: www.youtube.com (understandable ip addres text form)
+3. path: /watch
+4. query parameters: v=Nt-AsZh5woE, list=PLinedj3B30sDby4Al-i13hQJGQoRQDfPo, index=8 (seperated with '&')
+
+Query Paramaeter: key-value pairs that can be appended to the end of a url for passing additional, optional information like search options and filters
+
+*/
+
 import http from 'http';
 import fs from "fs";
+import url from "url";
 
 const server = http.createServer((req, res) => {
     const timestamp = Date.now();
@@ -12,11 +28,17 @@ const server = http.createServer((req, res) => {
     const hrs = date.getHours().toString();
     const mins = date.getMinutes().toString();
     const sec = date.getSeconds().toString();
-    
 
-    const formattedDate = `${year}/${month}/${day} - ${hrs}:${mins}:${sec} - ${req.url}`;
+    if (req.url == "/favicon.ico") { // to avoid "/favicon" route in log file
+            return res.end();
+    }
+    
+    const parsed_url = url.parse(req.url, true); // to parse the url 
+
+    const formattedDate = `${year}/${month}/${day}-${hrs}:${mins}:${sec} - ${parsed_url.path}`;
     fs.appendFile("logFile.txt", formattedDate + "\n", (err) => {
-        switch (req.url) {
+
+        switch (parsed_url.pathname) {
             case "/":
                 res.end("HOME PAGE");
                 break;
@@ -27,6 +49,11 @@ const server = http.createServer((req, res) => {
 
             case "/contact-us":
                 res.end("CONTACT PAGE");
+                break;
+
+            case "/users":
+                const user_name = parsed_url.query.u;
+                res.end(`Welcome Back, ${user_name}`);
                 break;
 
             default:
